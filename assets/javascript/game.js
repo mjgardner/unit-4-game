4485fe995fa8f0c50a8d6f26adb029a343860578
Mark Gardner <mark.gardner@endurance.com>
2018-12-10 16:13:10 -0600

    add game logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+crystalValue = [];
+
+function resetGame() {
+    // random number between 19 and 120 inclusive
+    $("#random-number").text(Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+
+    $("#game-score").text(0);
+
+    // crystals are worth between 1 and 12
+    for (var i = 0; i < 4; i++) {
+        crystalValue[i] = Math.floor(Math.random() * 12) + 1;
+    }
+}
+
+$(function() {
+    resetGame();
+
+    $(".crystal").click(function() {
+        var gameScore    = parseInt( $("#game-score").text() );
+        var randomNumber = parseInt( $("#random-number").text() );
+        gameScore += crystalValue[$(this)[0].id];
+        $("#game-score").text(gameScore);
+
+        if (gameScore == randomNumber) {
+            // you win!
+            $("#wins").text(parseInt( $("#wins").text() ) + 1);
+            resetGame();
+        }
+        else if (gameScore > randomNumber) {
+            // you lose!
+            $("#losses").text(parseInt( $("#losses").text() ) + 1);
+            resetGame();
+        }
+    });
+});
